1f473cf43117df216fc7205b3d32c1b23d7ae188
marcosdevelo <marcosgomezda@gmail.com>
2019-09-12 03:15:51 +0000

    eeeeee

--- hello-rigo/src/js/views/pickTeam.js
+++ hello-rigo/src/js/views/pickTeam.js
@@ -2,6 +2,7 @@ import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
+import "../../styles/pickteam.scss";
 
 export class PickTeam extends React.Component {
 	render() {
@@ -9,64 +10,83 @@ export class PickTeam extends React.Component {
 			<div className="tarjetas">
 				<Carousel>
 					<Carousel.Item>
-						<img
-							className="d-block w-100"
-							src="https://vignette.wikia.nocookie.net/logopedia/images/5/50/600px-Juventusstemma.png/revision/latest?cb=20120107135132"
-							alt="First slide"
-						/>
-						<Carousel.Caption>
-							<h3>First slide label</h3>
-							<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-						</Carousel.Caption>
+						<div className="carousel">
+							<img
+								className="d-block"
+								src="https://vignette.wikia.nocookie.net/logopedia/images/5/50/600px-Juventusstemma.png/revision/latest?cb=20120107135132"
+								alt="First slide"
+								width="225"
+								height="228"
+							/>
+							<Carousel.Caption>
+								<h3>First slide label</h3>
+							</Carousel.Caption>
+						</div>
 					</Carousel.Item>
 					<Carousel.Item>
-						<img
-							className="d-block w-100"
-							src="https://vignette.wikia.nocookie.net/logopedia/images/4/47/FC_Barcelona_%28crest%29.svg/revision/latest/scale-to-width-down/200?cb=20190425113731"
-							alt="Third slide"
-						/>
+						<div className="carousel">
+							<img
+								className="d-block "
+								src="https://vignette.wikia.nocookie.net/logopedia/images/4/47/FC_Barcelona_%28crest%29.svg/revision/latest/scale-to-width-down/200?cb=20190425113731"
+								alt="Third slide"
+								width="225"
+								height="228"
+							/>
 
-						<Carousel.Caption>
-							<h3>Second slide label</h3>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-						</Carousel.Caption>
+							<Carousel.Caption>
+								<h3>Second slide label</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+							</Carousel.Caption>
+						</div>
 					</Carousel.Item>
 					<Carousel.Item>
-						<img
-							className="d-block w-100"
-							src="https://vignette.wikia.nocookie.net/logopedia/images/9/98/Real_Madrid.png/revision/latest?cb=20161111231747"
-							alt="Third slide"
-						/>
+						<div className="carousel">
+							<img
+								className="d-block-real"
+								src="https://vignette.wikia.nocookie.net/logopedia/images/9/98/Real_Madrid.png/revision/latest?cb=20161111231747"
+								alt="Third slide"
+								width="225"
+								height="228"
+							/>
 
-						<Carousel.Caption>
-							<h3>Third slide label</h3>
-							<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-						</Carousel.Caption>
+							<Carousel.Caption>
+								<h3>Third slide label</h3>
+								<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+							</Carousel.Caption>
+						</div>
 					</Carousel.Item>
 
 					<Carousel.Item>
-						<img
-							className="d-block w-100"
-							src="https://vignette.wikia.nocookie.net/logopedia/images/4/49/Liverpool_FC_logo_%28introduced_2012%29.svg/revision/latest/scale-to-width-down/130?cb=20120705223744"
-							alt="Third slide"
-						/>
+						<div className="carousel">
+							<img
+								className="d-block"
+								src="https://vignette.wikia.nocookie.net/logopedia/images/4/49/Liverpool_FC_logo_%28introduced_2012%29.svg/revision/latest/scale-to-width-down/130?cb=20120705223744"
+								alt="Third slide"
+								width="225"
+								height="228"
+							/>
 
-						<Carousel.Caption>
-							<h3>Third slide label</h3>
-							<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-						</Carousel.Caption>
+							<Carousel.Caption>
+								<h3>Third slide label</h3>
+								<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+							</Carousel.Caption>
+						</div>
 					</Carousel.Item>
 					<Carousel.Item>
-						<img
-							className="d-block w-100"
-							src="https://vignette.wikia.nocookie.net/logopedia/images/3/36/Manchester_City_2016.png/revision/latest/scale-to-width-down/200?cb=20151229013856"
-							alt="Third slide"
-						/>
+						<div className="carousel">
+							<img
+								className="d-block"
+								src="https://vignette.wikia.nocookie.net/logopedia/images/3/36/Manchester_City_2016.png/revision/latest/scale-to-width-down/200?cb=20151229013856"
+								alt="Third slide"
+								width="225"
+								height="228"
+							/>
 
-						<Carousel.Caption>
-							<h3>Third slide label</h3>
-							<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-						</Carousel.Caption>
+							<Carousel.Caption>
+								<h3>Third slide label</h3>
+								<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+							</Carousel.Caption>
+						</div>
 					</Carousel.Item>
 				</Carousel>
 			</div>
