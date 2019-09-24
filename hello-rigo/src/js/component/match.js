fdac403f0e1beb258ce0c7030dd3040ff07cfbe3
marcosdevelo <marcosgomezda@gmail.com>
2019-09-24 21:37:09 +0000

    modal delete

--- hello-rigo/src/js/component/match.js
+++ hello-rigo/src/js/component/match.js
@@ -53,12 +53,6 @@ export class Match extends React.Component {
 						</div>
 					</div>
 				</div>
-				{/* Modal */}
-				{/* <div className="row pb-4">
-					<div className="col-4 offset-4 position-fixed">
-						<Fight /> */}
-				{/* </div> */}
-				{/* </div> */}
 				<div className="row">
 					<div className="col-6 ml-auto opponent">
 						<div className="row">
